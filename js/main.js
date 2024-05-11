34e198cbc1dfa34f296da4fa6d8d2402c45424d4
hassan hafez <hassanhafez86@gmail.com>
2024-05-11 18:48:44 +0300

    type animation + cursor

--- js/main.js
+++ js/main.js
@@ -1,176 +1,205 @@
-;(function () {
-	
-	'use strict';
+(function () {
+	("use strict");
 
-
-
-	// iPad and iPod detection	
-	var isiPad = function(){
-		return (navigator.platform.indexOf("iPad") != -1);
+	// iPad and iPod detection
+	var isiPad = function () {
+		return navigator.platform.indexOf("iPad") != -1;
 	};
 
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
-			(navigator.platform.indexOf("iPod") != -1)
-	    );
+	var isiPhone = function () {
+		return (
+			navigator.platform.indexOf("iPhone") != -1 ||
+			navigator.platform.indexOf("iPod") != -1
+		);
 	};
 
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
+	var fullHeight = function () {
+		$(".js-fullheight").css("height", $(window).height());
+		$(window).resize(function () {
+			$(".js-fullheight").css("height", $(window).height());
 		});
-
 	};
 
-	var burgerMenu = function() {
-
-		$('.js-colorlib-nav-toggle').on('click', function(event) {
+	var burgerMenu = function () {
+		$(".js-colorlib-nav-toggle").on("click", function (event) {
 			event.preventDefault();
 			var $this = $(this);
-			if( $('body').hasClass('menu-show') ) {
-				$('body').removeClass('menu-show');
-				$('#colorlib-main-nav > .js-colorlib-nav-toggle').removeClass('show');
+			if ($("body").hasClass("menu-show")) {
+				$("body").removeClass("menu-show");
+				$("#colorlib-main-nav > .js-colorlib-nav-toggle").removeClass("show");
 			} else {
-				$('body').addClass('menu-show');
-				setTimeout(function(){
-					$('#colorlib-main-nav > .js-colorlib-nav-toggle').addClass('show');
+				$("body").addClass("menu-show");
+				setTimeout(function () {
+					$("#colorlib-main-nav > .js-colorlib-nav-toggle").addClass("show");
 				}, 900);
 			}
-		})
+		});
+	};
+
+	var typeWriter = function (text, i, fnCallback) {
+		if (i < text.length) {
+			document.querySelector(".colorlib-logo .typing-text").textContent +=
+				text.charAt(i);
+			setTimeout(function () {
+				typeWriter(text, i + 1, fnCallback);
+			}, 100); // Adjust typing speed here (milliseconds)
+		} else {
+			setTimeout(function () {
+				deleteText(text, fnCallback);
+			}, 2000); // Adjust delay before deleting starts (milliseconds)
+		}
+	};
+
+	var deleteText = function (text, fnCallback) {
+		var currentText = document.querySelector(
+			".colorlib-logo .typing-text"
+		).textContent;
+		if (currentText.length > 0) {
+			var newText = currentText.slice(0, -1);
+			document.querySelector(".colorlib-logo .typing-text").textContent =
+				newText;
+			setTimeout(function () {
+				deleteText(text, fnCallback);
+			}, 100); // Adjust deletion speed here (milliseconds)
+		} else {
+			setTimeout(function () {
+				typeWriter(text, 0, fnCallback);
+			}, 1000); // Adjust delay before typing starts again (milliseconds)
+		}
+	};
+
+	var startTypingAnimation = function () {
+		var text = "<div>HELLO WORLD</div>";
+		typeWriter(text, 0, function () {
+			console.log("Animation finished.");
+		});
 	};
 
 	// Animations
-
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated');
-							} else {
-								el.addClass('fadeInUp animated');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '85%' } );
+		$(".animate-box").waypoint(
+			function (direction) {
+				if (direction === "down" && !$(this.element).hasClass("animated")) {
+					i++;
+					$(this.element).addClass("item-animate");
+					setTimeout(function () {
+						$("body .animate-box.item-animate").each(function (k) {
+							var el = $(this);
+							setTimeout(
+								function () {
+									var effect = el.data("animate-effect");
+									if (effect === "fadeIn") {
+										el.addClass("fadeIn animated");
+									} else {
+										el.addClass("fadeInUp animated");
+									}
+									el.removeClass("item-animate");
+								},
+								k * 200,
+								"easeInOutExpo"
+							);
+						});
+					}, 100);
+				}
+			},
+			{ offset: "85%" }
+		);
 	};
 
-
-	var counter = function() {
-		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
+	var counter = function () {
+		$(".js-counter").countTo({
+			formatter: function (value, options) {
+				return value.toFixed(options.decimals);
+			},
 		});
 	};
 
-	var counterWayPoint = function() {
-		if ($('#colorlib-counter').length > 0 ) {
-			$('#colorlib-counter').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
+	var counterWayPoint = function () {
+		if ($("#colorlib-counter").length > 0) {
+			$("#colorlib-counter").waypoint(
+				function (direction) {
+					if (direction === "down" && !$(this.element).hasClass("animated")) {
+						setTimeout(counter, 400);
+						$(this.element).addClass("animated");
+					}
+				},
+				{ offset: "90%" }
+			);
 		}
 	};
 
 	// Owl Carousel
-	var owlCarouselFeatureSlide = function() {
-		var owl = $('.owl-carousel1');
+	var owlCarouselFeatureSlide = function () {
+		var owl = $(".owl-carousel1");
 		owl.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:true,
-		   dots: false,
-		   autoHeight: true,
-		   responsive:{
-		      0:{
-		         items:1
-		      },
-		      600:{
-		         items:2
-		      },
-		      1000:{
-		         items:3
-		      }
-		   },
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
+			animateOut: "fadeOut",
+			animateIn: "fadeIn",
+			autoplay: true,
+			loop: true,
+			margin: 0,
+			nav: true,
+			dots: false,
+			autoHeight: true,
+			responsive: {
+				0: {
+					items: 1,
+				},
+				600: {
+					items: 2,
+				},
+				1000: {
+					items: 3,
+				},
+			},
+			navText: [
+				"<i class='icon-arrow-left3 owl-direction'></i>",
+				"<i class='icon-arrow-right3 owl-direction'></i>",
+			],
 		});
-		var owl2 = $('.owl-carousel');
+		var owl2 = $(".owl-carousel");
 		owl2.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:false,
-		   dots: true,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
+			animateOut: "fadeOut",
+			animateIn: "fadeIn",
+			autoplay: true,
+			loop: true,
+			margin: 0,
+			nav: false,
+			dots: true,
+			autoHeight: true,
+			items: 1,
+			navText: [
+				"<i class='icon-arrow-left3 owl-direction'></i>",
+				"<i class='icon-arrow-right3 owl-direction'></i>",
+			],
 		});
-		var owl3 = $('.owl-carousel3');
+		var owl3 = $(".owl-carousel3");
 		owl3.owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:false,
-		   dots: false,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		});	
+			animateOut: "fadeOut",
+			animateIn: "fadeIn",
+			autoplay: true,
+			loop: true,
+			margin: 0,
+			nav: false,
+			dots: false,
+			autoHeight: true,
+			items: 1,
+			navText: [
+				"<i class='icon-arrow-left3 owl-direction'></i>",
+				"<i class='icon-arrow-right3 owl-direction'></i>",
+			],
+		});
 	};
 
-	
-
-
 	// Document on load.
-	$(function(){
+	$(function () {
 		fullHeight();
 		burgerMenu();
 		counterWayPoint();
 		contentWayPoint();
 		owlCarouselFeatureSlide();
+
+		// Start the typing animation when the window loads
+		startTypingAnimation();
 	});
-
-
-}());
+})();
